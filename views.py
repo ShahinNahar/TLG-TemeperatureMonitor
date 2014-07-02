e13f73541cc38d8e238811a60b1366d5d2bb26ca
ShahinNahar <shahinnahar@yahoo.com>
2014-07-02 11:43:09 -0700

    updated view.py to run django

---
+++ views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from matplotlib import pylab
+from pylab import *
+
+import PIL
+import PIL.Image
+import StringIO
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def graph(request):
+   x = [1,2,3,4,5,6]
+   y = [5,2,6,8,2,7]
+   plot (x,y, linewidth =2)
+
+   xlabel('x axis')
+   ylabel ('y axis')
+   title ('simple graph')
+   grid(True)
+   #pylab.show()
+
+   buffer = StringIO.StringIO()
+   canvas = pylab.get_current_fig_manager().canvas
+   canvas.draw()
+   graphIMG = PIL.Image.fromstring("RGB", canvas.get_width_height(),canvas.tostring_rgb())
+   graphIMG.save(buffer,"PNG")
+   pylab.close()
+
+
+   return HttpResponse(buffer.getvalue(), mimetype = "image/png")
+
